520b76c56f649aed105287a0501843fc04afbbf0
Kevin Thomas <kevin@mytechnotalent.com>
2020-11-13 05:53:31 -0500

    Added files for Part 24

---
+++ Part_24_Bool_Primitive_Data_type/main.py
@@ -0,0 +1,14 @@
+is_hungry = True
+
+print(type(is_hungry))
+
+print(is_hungry > 0)
+print(is_hungry <= 0)
+
+# When you compare two values, the expression is eval to either True
+# or false
+print(100 > 1)
+print(100 < 10)
+print(42 == 42)
+print(42 != 42)
+print(42 == 4)
