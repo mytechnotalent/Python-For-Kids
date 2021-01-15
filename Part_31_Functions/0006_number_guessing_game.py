f58cb24d79758823c2c57afb2fce366f9dc24458
Kevin Thomas <kevin@mytechnotalent.com>
2021-01-15 13:04:52 -0500

    Update 0006_number_guessing_game.py

--- Part_31_Functions/0006_number_guessing_game.py
+++ Part_31_Functions/0006_number_guessing_game.py
@@ -15,14 +15,13 @@ def guess_number():
             raise ValueError
         return f_guess
     except ValueError:
-        print('\nRULES: Please enter a number between 1 and 9.')
-        return False
+        return '\nRULES: Please enter a number between 1 and 9.'
 
 
 def did_win(f_guess, f_correct_answer, f_turns_left):
     """Check player guess against the correct answer
 
-    Args:
+    Params:
         f_guess: int
         f_correct_answer: int
         f_turns_left: int
@@ -30,35 +29,41 @@ def did_win(f_guess, f_correct_answer, f_turns_left):
     Returns:
         int or False
     """
-    if f_turns_left > 1:
+    if f_turns_left >= 1:
         if f_guess > f_correct_answer:
-            print('HINT: Lower Than {0}'.format(f_guess))
-            return f_turns_left - 1
+            return 'HINT: Lower Than {0}'.format(f_guess), f_turns_left - 1
         elif f_guess < f_correct_answer:
-            print('HINT: Higher Than {0}'.format(f_guess))
-            return f_turns_left - 1
+            return 'HINT: Higher Than {0}'.format(f_guess), f_turns_left - 1
         else:
-            print('You won!')
-            return False
-    else:
-        return False
+            return 'You won!', None
 
 
 print('RULES: Guess a number between 1 and 9.')
 
 correct_answer = randint(1, 9)
 
-guess = 0
+guess = 1
 
 while guess != correct_answer:
-    guess = guess_number()
-
-    if guess:
-        turns_left = did_win(guess, correct_answer, turns_left)
-
-    if turns_left == 0:
+    if turns_left >= 1:
+        guess_response = guess_number()
+    else:
         print('The correct answer is {0}, let\'s play again!'.format(correct_answer))
         break
 
+    if isinstance(guess_response, str):
+        print(guess_response)
+    else:
+        if turns_left:
+            game_status, turns_left = did_win(guess_response, correct_answer, turns_left)
+            if game_status == 'You won!':
+                print(game_status)
+                break
+            else:
+                print(game_status)
+
     if guess and turns_left:
-        print('{0} turns left.\n'.format(turns_left))
+        if turns_left > 1:
+            print('{0} turns left.\n'.format(turns_left))
+        else:
+            print('{0} turn left.\n'.format(turns_left))
