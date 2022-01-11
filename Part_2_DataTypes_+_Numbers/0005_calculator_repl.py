4f9b2272f18d8092ade1a44dbb0dc823f4b958dc
Kevin Thomas <kevin@mytechnotalent.com>
2022-01-11 17:29:16 -0500

    Update 0005_calculator_repl.py

--- Part_2_DataTypes_+_Numbers/0005_calculator_repl.py
+++ Part_2_DataTypes_+_Numbers/0005_calculator_repl.py
@@ -1,12 +1,10 @@
 first_number = int(input('Enter First Number: '))
 second_number = int(input('Enter Second Number: '))
 
-
 my_addition = first_number + second_number
 my_subtraction = first_number - second_number
 my_multiplication = first_number * second_number
 my_division = first_number / second_number
-
 
 print('Addition = {0}'.format(my_addition))
 print('Subtraction = {0}'.format(my_subtraction))
