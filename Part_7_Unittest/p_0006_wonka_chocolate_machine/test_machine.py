857ecbe55687cb0d60c7d40b8ca0c574070ebfd5
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-02 10:42:42 -0500

    Update test_machine.py

--- Part_7_Unittest/p_0006_wonka_chocolate_machine/test_machine.py
+++ Part_7_Unittest/p_0006_wonka_chocolate_machine/test_machine.py
@@ -4,6 +4,10 @@ from machine import Machine
 
 
 class TestMachine(unittest.TestCase):
+    """
+    Test class to test machine module
+    """
+    
     def setUp(self):
         """
         setUp class
@@ -17,10 +21,8 @@ class TestMachine(unittest.TestCase):
         """
         # Returns
         return_1 = 'Total Money Collected: $0.00\n'
-
         # Calls
         string_1 = self.machine.stats()
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -33,13 +35,10 @@ class TestMachine(unittest.TestCase):
         m_quarters = 2
         m_dimes = 2
         m_nickels = 1
-
         # Returns
         return_1 = 0.75
-
         # Calls
         float_1 = self.machine.collect_money(max_value, m_quarters, m_dimes, m_nickels)
-
         # Asserts
         self.assertEqual(float_1, return_1)
 
@@ -52,13 +51,10 @@ class TestMachine(unittest.TestCase):
         m_quarters = 2000
         m_dimes = 2
         m_nickels = 1
-
         # Returns
         return_1 = 'Machine can\'t hold more than $100.00...  Dispensing coins inserted.\n'
-
         # Calls
         string_1 = self.machine.collect_money(max_value, m_quarters, m_dimes, m_nickels)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -71,13 +67,10 @@ class TestMachine(unittest.TestCase):
         m_quarters = 'invalid data'
         m_dimes = 2
         m_nickels = 1
-
         # Returns
         return_1 = 'Please enter valid currency...  Dispensing any coins inserted.\n'
-
         # Calls
         string_1 = self.machine.collect_money(max_value, m_quarters, m_dimes, m_nickels)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -89,13 +82,10 @@ class TestMachine(unittest.TestCase):
         price = 2.50
         self.__total_money_collected = 10.00
         __money_collected = 6.00
-
         # Returns
         return_1 = 'Change: $3.50\n'
-
         # Calls
         string_1 = self.machine.has_enough_money(price, __money_collected)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -107,13 +97,10 @@ class TestMachine(unittest.TestCase):
         price = 2.50
         self.__total_money_collected = 10.00
         __money_collected = 1.00
-
         # Returns
         return_1 = 'Insufficient funds...  Dispensing coins inserted.\n'
-
         # Calls
         string_1 = self.machine.has_enough_money(price, __money_collected)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -124,13 +111,10 @@ class TestMachine(unittest.TestCase):
         # Params
         shutdown_password = '8675309'
         entered_password = '8675309'
-
         # Returns
         return_1 = None
-
         # Calls
         none_1 = self.machine.shutdown_machine(shutdown_password, entered_password)
-
         # Asserts
         self.assertEqual(none_1, return_1)
 
@@ -141,13 +125,10 @@ class TestMachine(unittest.TestCase):
         # Params
         shutdown_password = '8675309'
         entered_password = 'not_correct_password'
-
         # Returns
         return_1 = 'YOU ARE NOT AUTHORIZED TO DISABLE THIS MACHINE!\n'
-
         # Calls
         string_1 = self.machine.shutdown_machine(shutdown_password, entered_password)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
