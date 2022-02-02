253dfe7abda0cdc508ee91701b38bc51a86103bc
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-02 11:19:30 -0500

    Update test_Grid.py

--- Part_7_Unittest/0014_escape_room/test_Grid.py
+++ Part_7_Unittest/0014_escape_room/test_Grid.py
@@ -1,10 +1,14 @@
 import unittest
 
-from Grid import Grid
-from EscapeRoomPlayer import EscapeRoomPlayer
+from grid import Grid
+from escape_room_player import EscapeRoomPlayer
 
 
 class TestGrid(unittest.TestCase):
+    """
+    Test class to test grid module
+    """
+    
     def setUp(self):
         """
         setUp class
@@ -21,13 +25,10 @@ class TestGrid(unittest.TestCase):
         self.player.dx = 0
         self.player.dy = 0
         player = self.player
-
         # Returns
         return_1 = '9\n'
-
         # Calls
         string_1 = self.grid.update(player)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
