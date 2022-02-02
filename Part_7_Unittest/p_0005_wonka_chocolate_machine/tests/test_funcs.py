eeb0a4fb4a6b8c78074d78d85d33bccd6be9821f
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-02 10:55:44 -0500

    Update test_funcs.py

--- Part_7_Unittest/p_0005_wonka_chocolate_machine/tests/test_funcs.py
+++ Part_7_Unittest/p_0005_wonka_chocolate_machine/tests/test_funcs.py
@@ -5,6 +5,10 @@ from ..data import CHOCOLATE_CHOICES
 
 
 class TestFuncs(unittest.TestCase):
+    """
+    Test class to test funcs module
+    """
+    
     def test_has_raw_materials(self):
         """
         test has_raw_materials functionality
@@ -12,7 +16,6 @@ class TestFuncs(unittest.TestCase):
         # Setup
         choice = 'dark'
         selection = CHOCOLATE_CHOICES[choice]
-
         # Params
         f_raw_materials = selection['ingredients']
         d_raw_materials = {
@@ -27,13 +30,10 @@ class TestFuncs(unittest.TestCase):
             'vanilla extract': 2,
             'Reese\'s Pieces': 15,
         }
-
         # Returns
         return_1 = True
-
         # Calls
         bool_1 = has_raw_materials(f_raw_materials, d_raw_materials)
-
         # Asserts
         self.assertEqual(bool_1, return_1)
 
@@ -44,7 +44,6 @@ class TestFuncs(unittest.TestCase):
         # Setup
         choice = 'dark'
         selection = CHOCOLATE_CHOICES[choice]
-
         # Params
         f_raw_materials = selection['ingredients']
         d_raw_materials = {
@@ -59,7 +58,6 @@ class TestFuncs(unittest.TestCase):
             'vanilla extract': 0,
             'Reese\'s Pieces': 15,
         }
-
         # Returns
         return_1 = 'Machine Needs Additional: sugar\n' \
             'Machine Needs Additional: butter\n' \
@@ -67,10 +65,8 @@ class TestFuncs(unittest.TestCase):
             'Machine Needs Additional: light corn syrup\n' \
             'Machine Needs Additional: sweetened condensed milk\n' \
             'Machine Needs Additional: vanilla extract\n'
-
         # Calls
         string_1 = has_raw_materials(f_raw_materials, d_raw_materials)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -83,13 +79,10 @@ class TestFuncs(unittest.TestCase):
         f_quarters = 22
         f_dimes = 10
         f_nickels = 5
-
         # Returns
         return_1 = 6.75
-
         # Calls
         float_1 = collect_money(f_max_value, f_quarters, f_dimes, f_nickels)
-
         # Asserts
         self.assertEqual(float_1, return_1)
 
@@ -102,13 +95,10 @@ class TestFuncs(unittest.TestCase):
         f_quarters = 2000
         f_dimes = 1
         f_nickels = 5
-
         # Returns
         return_1 = 'Machine can\'t hold more than $100.00...  Dispensing coins inserted.'
-
         # Calls
         string_1 = collect_money(f_max_value, f_quarters, f_dimes, f_nickels)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -121,13 +111,10 @@ class TestFuncs(unittest.TestCase):
         f_quarters = 'k'
         f_dimes = 1
         f_nickels = 5
-
         # Returns
         return_1 = 'Please enter valid currency.\n'
-
         # Calls
         string_1 = collect_money(f_max_value, f_quarters, f_dimes, f_nickels)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -139,13 +126,10 @@ class TestFuncs(unittest.TestCase):
         f_money_collected = 2.50
         f_chocolate_price = 2.25
         f_total_money_collected = 2.25
-
         # Returns
         return_1 = 'Change: $0.25\n', 4.5
-
         # Calls
         string_1 = has_enough_money(f_money_collected, f_chocolate_price, f_total_money_collected)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -157,13 +141,10 @@ class TestFuncs(unittest.TestCase):
         f_money_collected = 2.00
         f_chocolate_price = 2.25
         f_total_money_collected = 2.25
-
         # Returns
         return_1 = 'Insufficient funds...  Dispensing coins inserted.\n'
-
         # Calls
         string_1 = has_enough_money(f_money_collected, f_chocolate_price, f_total_money_collected)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -174,7 +155,6 @@ class TestFuncs(unittest.TestCase):
         # Setup
         choice = 'dark'
         selection = CHOCOLATE_CHOICES[choice]
-
         # Params
         f_chocolate_choice = 'dark'
         f_raw_materials = selection['ingredients']
@@ -190,13 +170,10 @@ class TestFuncs(unittest.TestCase):
             'vanilla extract': 2,
             'Reese\'s Pieces': 15,
         }
-
         # Returns
         return_1 = 'A dark chocolate bar dispensed!'
-
         # Calls
         string_1 = bake_chocolate_bar(f_chocolate_choice, f_raw_materials, d_raw_materials)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
@@ -218,7 +195,6 @@ class TestFuncs(unittest.TestCase):
             'Reese\'s Pieces': 15,
         }
         f_money_collected = 0.00
-
         # Returns
         return_1 = 'sugar 2 tablespoons remaining\n' \
             'butter 2 teaspoons remaining\n' \
@@ -230,10 +206,8 @@ class TestFuncs(unittest.TestCase):
             'vanilla extract 2 teaspoons remaining\n' \
             'Reese\'s Pieces 15 tablespoons remaining\n' \
             'Total Money Collected: $0.00\n'
-
         # Calls
         string_1 = stats(d_raw_materials, f_money_collected)
-
         # Asserts
         self.assertEqual(string_1, return_1)
 
